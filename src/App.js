cc905a24949a6abbe403aa765b9cf9c20ca098f7
Conejo Acosta <alberto.conejoacosta@abbott.com>
2022-12-22 10:42:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { Modal, ModalBody, ModalHeader, Button } from 'reactstrap';
+import React, { useState } from 'react';
+
+
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [open, setOpen] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Modal isOpen={open} toggle={() => setOpen(false)}>
+    <ModalHeader>
+      Felicidades kenneth
+    </ModalHeader>
+    <ModalBody>
+      Kenneth eres un carepicha
+    </ModalBody>
+    <Button onClick={() => setOpen(false)} >
+    cerrar
+  </Button>
+    
+  </Modal>
+    
+  <Button onClick={() => setOpen(true)} >
+    Abrir
+  </Button>
+
+  </div>
+
+  
+    
   );
 }
 
 export default App;
+
