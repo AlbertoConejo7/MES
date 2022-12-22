ba6bf5e89fbe4ad7c354e294069d0461f4e5a71c
Conejo Acosta <alberto.conejoacosta@abbott.com>
2022-12-22 15:05:59 -0600

    Segundo

--- src/App.js
+++ src/App.js
@@ -13,14 +13,101 @@ import {
   DropdownToggle,
   DropdownMenu,
   NavLink,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  Toast,
+  ToastBody,
+  ToastHeader,
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+  Breadcrumb,
+  BreadcrumbItem,
+
+
 } from 'reactstrap';
 
 import React, { useState } from 'react';
+
+const items = [
+  {
+    src: 'https://picsum.photos/id/123/1400/400',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    key: 1,
+  },
+  {
+    src: 'https://picsum.photos/id/456/1400/400',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    key: 2,
+  },
+  {
+    src: 'https://picsum.photos/id/678/1400/400',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    key: 3,
+  },
+];
+
+
+
+
+  
+
 
 
 function App() 
 {
 
+  const [open, setOpen] = useState('1');
+  const toggle2 = (id) => {
+    if (open === id) {
+      setOpen();
+    } else {
+      setOpen(id);
+    }
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
   
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -64,7 +151,104 @@ function App()
       </NavItem>
       
     </Nav>
+
+          <>
+      
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <a href="#">
+              Home
+            </a>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <a href="#">
+              Library
+            </a>
+          </BreadcrumbItem>
+          <BreadcrumbItem >
+            Data
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </>
+
+
+    <Carousel
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+           
+          >
+            <CarouselIndicators
+              items={items}
+              activeIndex={activeIndex}
+              onClickHandler={goToIndex}
+            />
+            {slides}
+            <CarouselControl
+              direction="prev"
+              directionText="Previous"
+              onClickHandler={previous}
+            />
+            <CarouselControl
+              direction="next"
+              directionText="Next"
+              onClickHandler={next}
+            />
+          </Carousel>
+          <div
+    className="p-3 my-2 rounded"
+    style={{
+      background: 'black'
+    }}
+  >
+    <Toast>
+      <ToastHeader>
+        BRK
+      </ToastHeader>
+      <ToastBody className='negra'>
+      Finalizamos este año con la implementación del 
+      sistema electrónico MES en dos áreas: BRK y Sub-Ensambles de EP.
+      </ToastBody>
+    </Toast>
+    
+  </div>
+  <div>
+      <Accordion open={open} toggle={toggle2}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">Accordion Item 1</AccordionHeader>
+          <AccordionBody accordionId="1">
+            <strong>This is the first item&#39;s accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default
+            variables. It&#39;s also worth noting that just about any HTML can
+            go within the <code>.accordion-body</code>, though the transition
+            does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader targetId="2">Accordion Item 2</AccordionHeader>
+          <AccordionBody accordionId="2">
+            <strong>This is the second item&#39;s accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default
+            variables. It&#39;s also worth noting that just about any HTML can
+            go within the <code>.accordion-body</code>, though the transition
+            does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader targetId="3">Accordion Item 3</AccordionHeader>
+          <AccordionBody accordionId="3">
+            <strong>This is the third item&#39;s accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default
+            variables. It&#39;s also worth noting that just about any HTML can
+            go within the <code>.accordion-body</code>, though the transition
+            does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+      </Accordion>
     </div>
+         
+    </div>
+    
     
       
     );
